c1d3679bda5ea6e9f10f325bc31d35c1716a4f16
Jana Al Abed <janaalabed0101@gmail.com>
2025-12-11 13:59:46 +0200

    fix: fix tailwind CSS configuration

--- app/page.js
+++ app/page.js
@@ -4,6 +4,11 @@ export default function Home() {
   return (
     <>
       <h1>Helloo</h1>
+      <div className="bg-green-500 text-white p-4 text-center">
+        Tailwind Works!
+      </div>
+
+
 
     </>
   );
