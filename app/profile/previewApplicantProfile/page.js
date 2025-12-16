5601c7916f24d537de5a9a802aad644d6ce917da
Jana Al Abed <janaalabed0101@gmail.com>
2025-12-16 19:47:09 +0200

    fix : adding and deleting some files

--- app/profile/previewApplicantProfile/page.js
+++ app/profile/previewApplicantProfile/page.js
@@ -1,7 +1,7 @@
 export default function ProfilePage() {
     return (
         <div className="min-h-screen flex items-center justify-center">
-            <h1 className="text-2xl font-bold text-gray-700">Profile Page (Coming Soon)</h1>
+            <h1 className="text-2xl font-bold text-gray-700"> Applicant Profile Page (Coming Soon)</h1>
         </div>
     );
 }
---
+++ app/profile/previewCompanyProfile/page.js
@@ -0,0 +1,7 @@
+export default function ProfilePage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <h1 className="text-2xl font-bold text-gray-700"> Company Profile Page (Coming Soon)</h1>
+        </div>
+    );
+}
